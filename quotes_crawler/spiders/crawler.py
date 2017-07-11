004370b9aab3fb0e065a3280319d2f2100cb4176
qathy <thravran.qathy@gmail.com>
2017-07-11 14:41:21 +0200

    Create crawler.py
    new crawler which explores all the pages linked through 'next' button

---
+++ quotes_crawler/spiders/crawler.py
@@ -0,0 +1,20 @@
+import scrapy
+
+class QuotesToscrape(scrapy.Spider):
+    name = "quotes"
+    allowed_domains = [u'toscrape.com']
+    start_urls = [u'http://quotes.toscrape.com/']
+    
+    def parse(self, response):
+        self.log('I just visited: ' + response.url)
+        for quote in response.css('div.quote'):
+            item = {
+               'author_name': quote.css('small.author::text').extract_first(),
+               'text': quote.css('span.text::text').extract_first(),
+               'tags': quote.css('a.tag::text').extract(),
+              }
+            yield item
+        next_page_url = response.css('li.next > a::attr(href)').extract_first()
+        if next_page_url:
+            next_page_url = response.urljoin(next_page_url)
+            yield scrapy.Request(url=next_page_url, callback=self.parse)
