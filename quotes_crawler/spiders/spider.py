d24e95174a3539b59a338286d7f98d7c80e06964
qathy <thravran.qathy@gmail.com>
2017-07-11 14:15:16 +0200

    Several items at once
    Scrap main page instead of random quote to illustrate tutorial on several items selection.

--- quotes_crawler/spiders/spider.py
+++ quotes_crawler/spiders/spider.py
@@ -3,12 +3,14 @@ import scrapy
 class QuotesToscrape(scrapy.Spider):
     name = "quotes"
     allowed_domains = [u'toscrape.com']
-    start_urls = [u'http://quotes.toscrape.com/random']
+    start_urls = [u'http://quotes.toscrape.com/']
     
     def parse(self, response):
         self.log('I just visited: ' + response.url)
-        yield {
-               'author_name': response.css('small.author::text').extract_first(),
-               'text': response.css('span.text::text').extract_first(),
-               'tags': response.css('a.tag::text').extract(),
+        for quote in response.css('div.quote'):
+            item = {
+               'author_name': quote.css('small.author::text').extract_first(),
+               'text': quote.css('span.text::text').extract_first(),
+               'tags': quote.css('a.tag::text').extract(),
               }
+            yeld item
