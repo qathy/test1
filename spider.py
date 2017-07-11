06abe4fafa818adbc00fb791267f86768b1ecbcb
qathy <thravran.qathy@gmail.com>
2017-07-11 11:22:03 +0200

    Create spider.py
    from Scrapy tutorial

---
+++ spider.py
@@ -0,0 +1,14 @@
+import scrapy
+
+class QuotesToscrape(scrapy.Spider):
+    name = "quotes"
+    allowed_domains = [u'toscrape.com']
+    start_urls = [u'http://quotes.toscrape.com/random']
+	
+	def parse(self, response):
+		self.log('I just visited: ' + response.url)
+		yield (
+			'author_name': response.css('small.author::text).extract_first(),
+			'text': response.css('span.text::text).extract_first(),
+			'tags': response.css('a.tag::text).extract(),
+		)
