e62a4d92ba0d1f2527516e3161ee7a1daef375ec
Luc Perthuis <lps@PRUNE.vanad.local>
2017-07-11 13:32:30 +0200

    fix indentation which induced failed build

--- quotes_crawler/spiders/spider.py
+++ quotes_crawler/spiders/spider.py
@@ -4,11 +4,11 @@ class QuotesToscrape(scrapy.Spider):
     name = "quotes"
     allowed_domains = [u'toscrape.com']
     start_urls = [u'http://quotes.toscrape.com/random']
-	
-	def parse(self, response):
-		self.log('I just visited: ' + response.url)
-		yield (
-			'author_name': response.css('small.author::text').extract_first(),
-			'text': response.css('span.text::text').extract_first(),
-			'tags': response.css('a.tag::text').extract(),
-		)
+    
+    def parse(self, response):
+        self.log('I just visited: ' + response.url)
+        yield (
+               'author_name': response.css('small.author::text').extract_first(),
+               'text': response.css('span.text::text').extract_first(),
+               'tags': response.css('a.tag::text').extract(),
+              )
