42bb5bf8031f6523a2a9b219d0ce0e3d494b4be6
qathy <thravran.qathy@gmail.com>
2017-07-12 09:35:44 +0200

    Create tags.py
    This spider should visit once each tag page and extract from there some tag information, including the number of quotes.
    It also grab the first quote of last page for each tag.
    => 2 different sorts of items

---
+++ quotes_crawler/spiders/tags.py
@@ -0,0 +1,45 @@
+import scrapy
+
+class QuotesToscrape(scrapy.Spider):
+    name = "all_tags"
+    allowed_domains = [u'toscrape.com']
+    start_urls = [u'http://quotes.toscrape.com/']
+    all_tags = {}
+    
+    def parse(self, response):
+        self.log('I just visited: ' + response.url)
+        quotes = response.css('div.quote')
+        # manage page tag if we are on a page tag
+        tag_head = response.css('h3 > a::text').extract_first()
+        next_page_url = response.css('li.next > a::attr(href)').extract_first()
+        if tag_head:
+            tag = all_tags[tag_head]
+            tag['visited'] = true
+            tag['#quotes'] = tag['#quotes'] + quotes.count()
+            if not next_page_url
+                yield tag
+                sample = quotes[0]
+                yield {
+                       'author_name': sample.css('small.author::text').extract_first(),
+                       'text': sample.css('span.text::text').extract_first(),
+                      }
+        # collect tags from any visited page
+        for quote in quotes:
+            tags = quote.css('a.tag')
+            for tag in tags:
+                tag_text = tag.css('a::text').extract_first()
+                if not tag_text in all_tags:
+                    all_tags[tag_text] = {
+                                          'url': tag.css('a::attr(href)').extract_first(),
+                                          'visited': false,
+                                          '#quotes': 0,
+                                         }
+        # visit one unvisited tag page
+        for tag in all_tags:
+            if not tag['visited']:
+                next_page_url = response.urljoin(tag['url'])
+                yield scrapy.Request(url=next_page_url, callback=self.parse)
+        # visit next page if there's a next button
+        if next_page_url:
+            next_page_url = response.urljoin(next_page_url)
+            yield scrapy.Request(url=next_page_url, callback=self.parse)
